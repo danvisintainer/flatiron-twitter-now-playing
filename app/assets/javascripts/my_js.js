32836209b702b9b8b7101e85f0f8028b20f25c50
danvisintainer <dan@danvisintainer.com>
2015-04-02 17:16:03 -0400

    add working select-all checkbox

--- app/assets/javascripts/my_js.js
+++ app/assets/javascripts/my_js.js
@@ -4,4 +4,22 @@ $(function() {
       keyboard: true
     });
   });
+
+  $(".spotify_checkbox").change(function() {
+    if(this.checked) {
+      console.log("Checked");
+    }
+  });
+
+  $('#select_all').click(function(event) {  //on click 
+    if(this.checked) { // check select status
+      $('.spotify_checkbox').each(function() { //loop through each checkbox
+        this.checked = true;  //select all checkboxes with class "checkbox1"               
+      });
+    }else{
+      $('.spotify_checkbox').each(function() { //loop through each checkbox
+        this.checked = false; //deselect all checkboxes with class "checkbox1"                       
+      });         
+    }
+  });
 });
